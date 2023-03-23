cd1f43f1f375754cfc1b40f5995d11cff86b3d5e
Coronel-08 <dannsmon520@gmail.com>
2023-03-23 16:08:13 -0300

    first commit

---
+++ app/src/main/java/com/example/app_organizao/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.app_organizao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btCadastro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btCadastro =(Button) findViewById(R.id.btCadastro);
+        btCadastro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.btCadastro){
+            Intent intenção = new Intent(this, cadastro.class);
+            startActivity(intenção);
+        }
+    }
+}
